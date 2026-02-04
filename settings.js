465e12be769669db8c513a92a5aa14b0f9912add
hafeezbalock <63083724+hafeezbalock@users.noreply.github.com>
2026-02-04 04:20:06 -0800

    Update bot settings for Hafeez Bot

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Hafeez Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Hafeez Bot",
+  botOwner: 'Baloch', // Your name
+  ownerNumber: '923003534702', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.6",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/mruniquehacker/Hafeezbot-MD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
